0cffaa6980fb7da820503c1edaf97ab7faa5c7a6
Arthur <peace_a99@mail.ru>
2025-01-18 09:03:06 +0200

    I have changed

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -2,23 +2,27 @@ package web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.dao.CarDAO;
 import web.model.Car;
-import web.serivice.CarService;
 
 import java.util.List;
 
 
 @Controller
 public class CarsController {
+    private  CarDAO carDAO;
+
     @Autowired
-    private CarService carService;
+    public void CarController(CarDAO carDAO) {
+        this.carDAO = carDAO;
+    }
 
     @GetMapping("/cars")
-    public String Getcars(ModelMap model, @RequestParam(value = "count",defaultValue = "5") int count) {
-        model.addAttribute("cars", carService.getCarList(count));
-        return "car";
+    public String getCars(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
+        List<Car> cars = carDAO.getCarList(count);
+        model.addAttribute("cars", cars);
+        return "cars"; // Имя шаблона (например, cars.html)
     }
 }
